472c88892611e2badfae946d5cea31596dbee019
Taeseok Sim <ct1799@hanmail.net>
2022-08-26 21:35:59 +0900

    react: to do app ; optimize

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,55 +1,50 @@
 import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import List from './List';
 
-export default function Lists({ todoData, setTodoData }) {
-  const btnStyle = {
-    color: '#fff',
-    border: 'none',
-    padding: '5px 9px',
-    borderRadius: '50%',
-    cursor: 'pointer',
-    float: 'right',
-  };
-
-  const getStyle = (completed) => {
-    return {
-      padding: '10px',
-      borderBottom: '1px #ccc dotted',
-      textDecoration: completed ? 'line-through' : 'none',
-    };
-  };
-
-  const handleClick = (id) => {
-    let newTodoData = todoData.filter((data) => data.id !== id);
+const Lists = React.memo(({ todoData, setTodoData }) => {
+  const handleEnd = (result) => {
+    if (!result.destination) return;
+    const newTodoData = todoData;
+    const [reorderedItem] = newTodoData.splice(result.source.index, 1);
+    newTodoData.splice(result.destination.index, 0, reorderedItem);
     setTodoData(newTodoData);
-  };
-
-  const handleCompleteChange = (id) => {
-    let newTodoData = todoData.map((data) => {
-      if (data.id === id) {
-        data.completed = !data.completed;
-      }
-      return data;
-    });
-    setTodoData(newTodoData);
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
   };
 
   return (
-    <div>
-      {todoData.map((data) => (
-        <div style={getStyle(data.completed)} key={data.id}>
-          <p>
-            <input
-              type="checkbox"
-              onChange={() => handleCompleteChange(data.id)}
-              defaultChecked={data.completed}
-            />{' '}
-            {data.title}
-            <button style={btnStyle} onClick={() => handleClick(data.id)}>
-              x
-            </button>
-          </p>
-        </div>
-      ))}
-    </div>
+    <DragDropContext onDragEnd={handleEnd}>
+      <div>
+        <Droppable droppableId={'to-dos'}>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {todoData.map((data, index) => (
+                <Draggable
+                  key={data.id}
+                  draggableId={data.id.toString()}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <List
+                      key={data.id}
+                      id={data.id}
+                      title={data.title}
+                      completed={data.completed}
+                      todoData={todoData}
+                      setTodoData={setTodoData}
+                      provided={provided}
+                      snapshot={snapshot}
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
   );
-}
+});
+
+export default Lists;
